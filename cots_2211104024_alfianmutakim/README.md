ae48e10586fecacd25407b3348a8531ad53ceb00
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-01-03 13:48:19 +0700

    Pembuatan Project

---
+++ cots_2211104024_alfianmutakim/README.md
@@ -0,0 +1,3 @@
+# cots_2211104024_alfianmutakim
+
+A new Flutter project.
---
+++ cots_2211104024_alfianmutakim/android/app/src/main/kotlin/com/example/cots_2211104024_alfianmutakim/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cots_2211104024_alfianmutakim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
